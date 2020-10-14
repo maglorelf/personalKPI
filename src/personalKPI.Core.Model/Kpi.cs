a1a373f6c1cbceaee1220d4260f4c3c7d17679d1
Marcos Javier Martínez Ramos <maglor_elf@yahoo.es>
2020-10-15 00:56:48 +0200

    #4 Basic project model

---
+++ src/personalKPI.Core.Model/Kpi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace personalKPI.Core.Model
+{
+  public   class Kpi
+    {
+        public int Id { get; set; }
+        public int KpiId { get; set; }
+        public int UserId { get; set; }
+        public KpiTemplate KpiTemplate { get; set; }
+        public User User { get; set; }
+
+
+    }
+}
---
+++ src/personalKPI.Core.Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace personalKPI.Core.Model
+{
+ public   class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Username { get; set; }
+    }
+}
